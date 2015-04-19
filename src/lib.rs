81906612b980c9a801c581df12668ee5c07381e9
Christopher James Halse Rogers <raof@ubuntu.com>
2015-04-20 08:40:54 +1000

    Add start of test fixture creation infrastructure

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,56 @@
 #![feature(libc)]
+#![feature(rustc_private)]
 
 extern crate libc;
-use libc::{c_int, c_ulong};
+use libc::{c_int, c_ulong, c_void};
+
+extern crate rustc_back;
 
 use std::io::Error;
 
 #[link(name = "c")]
 extern {
     fn ioctl(fd : c_int, request : c_ulong, ...) -> c_int;
+}
+
+#[cfg(test)]
+struct HotDataSupportingFixture {
+    base_dir : std::path::PathBuf,
+    fs_image_file : std::fs::File,
+    fs_image_dir : std::path::PathBuf
+}
+
+#[cfg(test)]
+impl Drop for HotDataSupportingFixture {
+    fn drop(&mut self) {
+        assert!(Command::new("sudo").arg("umount").arg(&self.base_dir.as_os_str()).status().is_ok());
+        assert!(std::fs::remove_dir_all(&self.base_dir).is_ok());
+        assert!(std::fs::remove_dir_all(&self.fs_image_dir).is_ok());
+    }
+}
+
+use std::process::Command;
+
+#[cfg(test)]
+impl HotDataSupportingFixture {
+    fn new() -> HotDataSupportingFixture {
+        let tmpdir = rustc_back::tempdir::TempDir::new("hot-data-test").unwrap();
+        let image_path = tmpdir.path().join("btrfs-image");
+        let mount_dir = tmpdir.path().join("btrfs-mount-point");
+
+        let image_file = std::fs::File::create(&image_path).unwrap();
+        assert!(image_file.set_len(1024*1024*100).is_ok());
+        assert!(image_file.sync_all().is_ok());
+
+        assert!(std::fs::create_dir(&mount_dir).is_ok());
+
+        assert!(Command::new("mkfs.btrfs").arg(&image_path.as_os_str()).status().is_ok());
+        assert!(Command::new("sudo").arg("mount").arg(&image_path.as_os_str()).arg(&mount_dir.as_os_str()).status().is_ok());
+
+        HotDataSupportingFixture { base_dir : mount_dir,
+                                   fs_image_file : image_file,
+                                   fs_image_dir : tmpdir.into_path() }
+    }
 }
 
 #[test]
